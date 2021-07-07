b9fb7325ed5259ebaa8db40ef159cd9b48684461
ellefry <ellefry@qq.com>
2021-07-07 22:17:47 +0800

    initial

---
+++ readme.txt
@@ -0,0 +1,5 @@
+https://armno.in.th/2020/02/26/cypress-angular-integration-testing/
+
+1. npm install start-server-and-test
+
+2. npm run cy
---
+++ tests/e2e/integration/home-page.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+describe('Home Page', () => {
+  it('should display the app name on the home page', () => {
+    cy.visit('/'); // go to the home page
+
+    // get the rocket element and verify that the app name is in it
+    //cy.get('.highlight-card')
+    //  .should('contain.text', 'angular-cypress');
+
+    cy.title().should('include', 'Tour of Heroes')
+  });
+});
